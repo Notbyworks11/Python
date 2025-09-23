fd22632c0e221082a26af1b6732cf1d161199006
Notbyworks11 <Kwadwoappiah479@gmail.com>
2025-09-23 13:49:36 +0100

    OOP

---
+++ OOP.py
@@ -0,0 +1,9 @@
+class Animal():
+    def __init__(self, name,sound):
+        self.name - name 
+        self.sound = sound 
+
+    def speak(self):
+        return f"{self.name} says {self.sound}"
+    
+

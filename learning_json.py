a2607b549d10b459ef0984285d5c43e09fbd3d53
Notbyworks11 <Kwadwoappiah479@gmail.com>
2025-09-30 00:09:09 +0100

    creating_json_file

--- learning_json.py
+++ learning_json.py
@@ -1,3 +1,26 @@
 import json
 
 #learning about json files now
+dog_data = {
+  "name": "Frieda",
+  "is_dog": True,
+  "hobbies": ["eating", "sleeping", "barking",],
+  "age": 8,
+  "address": {
+    "work": None,
+    "home": ("Berlin", "Germany",),
+  },
+  "friends": [
+    {
+      "name": "Philipp",
+      "hobbies": ["eating", "sleeping", "reading",],
+    },
+    {
+      "name": "Mitch",
+      "hobbies": ["running", "snacking",],
+    },
+  ],
+}
+
+with open('dog_data.json', 'w', encoding = "utf-8") as write_file:
+    json.dump(dog_data, write_file)
